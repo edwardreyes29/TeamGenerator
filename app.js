fe7cf65f7c437dd686de484b1610b55aa62d0e68
Edward Reyes <reyes.edwardf@gmail.com>
2020-08-21 21:12:34 -0700

    called render function and wrote html to team.html

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require("path");
 const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
+console.log(OUTPUT_DIR)
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
@@ -117,45 +118,65 @@ var employees = [];
 
 async function getEmployees() {
     let memberChoice = await inquirer.prompt(questions)
-    .then(function(response) {
-        const manager = new Manager(response.name, response. id, response.email, response.office_number);
-        employees.push(manager);
-        return response.member_choice;
-    })
+        .then(function (response) {
+            const manager = new Manager(response.name, response.id, response.email, response.office_number);
+            employees.push(manager);
+            return response.member_choice;
+        })
 
     while (memberChoice !== 'I don\'t want any more team members') {
         if (memberChoice === 'Intern') {
             memberChoice = await inquirer.prompt(internQuestions)
-            .then(function(response) {
-                const intern = new Intern(response.name, response. id, response.email, response.school);
-                employees.push(intern);
-                return response.member_choice;
-            })
+                .then(function (response) {
+                    const intern = new Intern(response.name, response.id, response.email, response.school);
+                    employees.push(intern);
+                    return response.member_choice;
+                })
         } else {
             memberChoice = await inquirer.prompt(engineerQuestions)
-            .then(function(response) {
-                const engineer = new Engineer(response.name, response. id, response.email, response.github);
-                employees.push(engineer);
-                return response.member_choice;
-            })
+                .then(function (response) {
+                    const engineer = new Engineer(response.name, response.id, response.email, response.github);
+                    employees.push(engineer);
+                    return response.member_choice;
+                })
         }
     }
-
-    console.log(employees);
+    // After the user has input all employees desired, call the `render` function (required
+    // above) and pass in an array containing all employee objects; the `render` function will
+    // generate and return a block of HTML including templated divs for each employee!
+    const renderedHTML = render(employees)
+    if (fs.existsSync(OUTPUT_DIR)) {
+        console.log('The path exists.');
+        writeHTML(renderedHTML)
+    }else {
+        console.log("File does not exist")
+        fs.mkdir(OUTPUT_DIR, { recursive: true }, (err) => {
+            if (err) throw err;
+            writeHTML(renderedHTML)
+        });
+    }
+    
 }
 // function call to initialize program
 getEmployees();
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
+function writeHTML(renderedHTML) {
+    fs.writeFile(outputPath, renderedHTML, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Success!");
+    })
+}
+
+
+
+
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
